9b26b368755af174764a0cbf018afa17b9a7ef44
premiuncarrotshop <benqu94@naver.com>
2021-11-16 19:25:06 +0900

    rendering optimization using react-virtualized

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import './TodoList.scss';
 import TodoListItem from './TodoListItem';
+import {List} from 'react-virtualized';
 
 const TodoList = ({todos, onRemove, onToggle}) => {
 
+  const rowRenderer = useCallback(({index, key, style}) => {
+    const todo = todos[index];
+
+    return (
+      <TodoListItem todo={todo} key={key} onRemove={onRemove} onToggle={onToggle} style={style}/>
+    )
+  }, [todos, onRemove, onToggle]);
+
   return (
     <div className="TodoList">
-      {todos.length === 0 ? <span className="nothing">일정을 추가해 보세요 !</span> : todos.map(todo => <TodoListItem key={todo.id} todo={todo} onRemove={onRemove} onToggle={onToggle} />)}
+      {todos.length === 0 ? <span className="nothing">일정을 추가해 보세요 !</span> : <List className="TodoList"
+                                                                                          width={512}
+                                                                                          height={513} 
+                                                                                          rowCount={todos.length} 
+                                                                                          rowHeight={67.58} 
+                                                                                          rowRenderer={rowRenderer} 
+                                                                                          list={todos} 
+                                                                                          style={{outline: 'none'}} />}
     </div>
   );
 };
 
-export default TodoList;
+export default React.memo(TodoList);
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -3,18 +3,20 @@ import classNames from 'classnames';
 import './TodoListItem.scss';
 import {MdCheckBox, MdCheckBoxOutlineBlank, MdRemoveCircleOutline} from "react-icons/md";
 
-const TodoListItem = ({todo, onRemove, onToggle}) => {
+const TodoListItem = ({todo, onRemove, onToggle, style}) => {
   const {id, text, checked} = todo;
 
   return (
-    <div className="TodoListItem">
-      <div className={classNames('checkbox', {checked})} onClick={() => onToggle(id)}>
-        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-        <div className="text">{text}</div>
+    <div className="TodoListItem-virtualized" style={style}>
+      <div className="TodoListItem">
+        <div className={classNames('checkbox', {checked})} onClick={() => onToggle(id)}>
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+          <div className="text">{text}</div>
+        </div>
+        <div className="remove" onClick={() => onRemove(id)}><MdRemoveCircleOutline /></div>
       </div>
-      <div className="remove" onClick={() => onRemove(id)}><MdRemoveCircleOutline /></div>
     </div>
   );
 };
 
-export default TodoListItem;
+export default React.memo(TodoListItem);
